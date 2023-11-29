912707345f6822f8538101d903acd968273806a5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-29 22:22:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
 
+const circleOne = document.querySelector('.circle-one')
+const circleTwo = document.querySelector('.circle-two')
 
+// Event Listeners
+window.addEventListener('scroll', () => {
+    // Difference between the scroll bar and the window
+    let scroll = window.scrollY;
 
-
-
-
-
-
+    // Dynamic style for circles
+    circleOne.style.clipPath = `circle(${(150 + scroll) * 0.8}px at 0 0)`
+    circleTwo.style.clipPath = `circle(${(150 + scroll) * 0.8}px at 100% 100%)`
+});
